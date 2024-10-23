31e5417f55c9bcbd98ff47ad6db89fce94e95f46
Tesh <54969187+TeshShin@users.noreply.github.com>
2024-10-23 17:18:47 +0900

    [Silver III] Title: 1로 만들기, Time: 4 ms, Memory: 5804 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -11,8 +11,8 @@ int main() {
 	for (int i = 4; i <= N; i++) {
 		int temp = table[i - 1] + 1; // 1 빼기로 + 1하고 이전 꺼 최소 횟수로
 
-		if (i % 3 == 0) temp = min(temp, table[i / 3] + 1); // 3나눠 떨어지는 횟수
-		if (i % 2 == 0) temp = min(temp, table[i / 2] + 1); // 2나눠 떨어지는 횟수
+		if (i % 3 == 0) temp = min(temp, table[i / 3] + 1); // 3나눠 떨어질때 횟수
+		if (i % 2 == 0) temp = min(temp, table[i / 2] + 1); // 2나눠 떨어질때 횟수
 		table[i] = temp;
 	}
 	cout << table[N];
