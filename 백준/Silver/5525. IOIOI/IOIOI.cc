ea7834fee6d3dced79c195ab120ac763619685cd
Tesh <54969187+TeshShin@users.noreply.github.com>
2024-11-14 12:45:41 +0900

    [Silver I] Title: IOIOI, Time: 12 ms, Memory: 7588 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.cc
+++ 백준/Silver/5525. IOIOI/IOIOI.cc
@@ -2,27 +2,43 @@
 #include<string>
 using namespace std;
 
+int str[1000001];
+
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
 	cout.tie(NULL);
 	int N, M, length = 1;
 	cin >> N >> M;
-	string p = "I";
-	for (int i = 0; i < N; i++) {
-		p += "OI";
-		length += 2;
-	}
 	string s;
 	cin >> s;
+
 	int count = 0;
-	for (int i = 0; i < M; i++) {
-		if (s[i] == 'I') {
-			if (s.substr(i, length) == p) {
-				count++;
-			}
-		}
-	}
-	cout << count;
+    for (int i = 0; i < M; i++) {
+        switch (i) {
+        case 0:
+            str[i] = (s[i] == 'I') ? 1 : -1;
+            break;
+        case 1:
+            str[i] = (s[i] == 'I') ? 1 : -1;
+            break;
+        default:
+            if (s[i] == 'I') {
+                if (str[i - 1] == -1 && str[i - 2] > 0) {
+                    str[i] = str[i - 2] + 1;
+                    if (str[i] > N) {
+                        count++;
+                    }
+                }
+                    
+                else
+                    str[i] = 1;
+            }
+            else
+                str[i] = -1;
+            break;
+        }
+    }
+    cout << count;
 	return 0;
 }
