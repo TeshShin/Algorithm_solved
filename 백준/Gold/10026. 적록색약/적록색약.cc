191271e95a241b58ccc3eb93b2b33c7fb388a3b8
Tesh <54969187+TeshShin@users.noreply.github.com>
2024-11-21 01:09:20 +0900

    [Gold V] Title: 적록색약, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/10026. 적록색약/적록색약.cc
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<queue>
+#include<stack>
+using namespace std;
+
+int d[4] = { -1, 1, 0, 0 };
+int N;
+
+
+int bfs(char map[100][100]) {
+	bool visit[100][100] = { false };
+	int cnt = 0;
+	for(int i = 0; i < N; i++){
+		for (int j = 0; j < N; j++) {
+			if (!visit[i][j]) {
+				queue<int> qx;
+				queue<int> qy;
+				qx.push(i);
+				qy.push(j);
+				visit[i][j] = true;
+				while (!qx.empty()) {
+					int x = qx.front();
+					int y = qy.front();
+					qx.pop();
+					qy.pop();
+					for (int k = 0; k < 4; k++) {
+						int nx = x + d[k];
+						int ny = y + d[3 - k];
+						if (0 <= nx && 0 <= ny && nx < N && ny < N) {
+							if (!visit[nx][ny] && map[nx][ny] == map[x][y]) {
+								qx.push(nx);
+								qy.push(ny);
+								visit[nx][ny] = true;
+							}
+						}
+					}
+				}
+				cnt++;
+			}
+		}
+	}
+	return cnt;
+}
+
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N;
+	char map[100][100];
+	char weakmap[100][100];
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			cin >> map[i][j];
+			if (map[i][j] == 'G') {
+				weakmap[i][j] = 'R';
+			}
+			else {
+				weakmap[i][j] = map[i][j];
+			}
+		}
+	}
+	cout << bfs(map) << " " << bfs(weakmap);
+
+	return 0;
+}
