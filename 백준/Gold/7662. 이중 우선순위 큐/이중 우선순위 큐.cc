a4cacc3aa34f65c9ba429ec758d62c4cd0613246
Tesh <54969187+TeshShin@users.noreply.github.com>
2024-11-29 15:48:34 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1312 ms, Memory: 49016 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -1,48 +1,40 @@
 #include<iostream>
-#include<queue>
-#include<map>
+#include <vector>
+#include <set>
 using namespace std;
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
-	int T;
-	cin >> T;
-	while (T--) {
-		priority_queue<int, vector<int>, less<int>> pq_max;
-		priority_queue<int, vector<int>, greater<int>> pq_min;
-		map<int, int> cntmp;
-		int N;
-		cin >> N;
-		while (N--) {
-			char command;
-			int num;
-			cin >> command;
-			cin >> num;
-			if (command == 'I') {
-				pq_max.push(num);
-				pq_min.push(num);
-				cntmp[num]++;
-			}
-			else if (num == 1 && !pq_max.empty()){
-				cntmp[pq_max.top()]--;
-				pq_max.pop();
-			}
-			else if(num == -1 && !pq_min.empty()){
-				cntmp[pq_min.top()]--;
-				pq_min.pop();
-			}
-			// 원소가 유효하지 않으면 빼줘야한다.
-			while (!pq_max.empty() && !cntmp[pq_max.top()]) pq_max.pop();
-			while (!pq_min.empty() && !cntmp[pq_min.top()]) pq_min.pop();
-		}
-		if (pq_min.empty() || pq_max.empty()) {
-			cout << "EMPTY" << '\n';
-		}
-		else {
-			cout << pq_max.top() << " " << pq_min.top() << '\n';
-		}
-	}
-	return 0;
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    char c; int n;
+ 
+    int T;
+    cin >> T;
+    while (T--) {
+        multiset<int, less<int>> ms; // 오름차순
+        int N;
+        cin >> N;
+        while (N--) {
+            cin >> c;
+            cin >> n;
+            if (c == 'I')
+                ms.insert(n);
+            else {
+                if (n == 1 && !ms.empty())
+                    ms.erase(--ms.end());
+                else if (n == -1 && !ms.empty())
+                    ms.erase(ms.begin());
+            }
+        }
+        if (ms.empty()) {
+            cout << "EMPTY" << '\n';
+        }
+        else {
+            cout << *(--ms.end()) << " " << *ms.begin() << '\n';
+        }
+    }
+    
+    return 0;
 }
