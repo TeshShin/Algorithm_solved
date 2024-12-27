ed39bd6a4dcfc6dcea966d31367e5c5a1b903304
Tesh <54969187+TeshShin@users.noreply.github.com>
2024-12-27 18:43:49 +0900

    [Silver III] Title: N과 M (2), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cc
@@ -0,0 +1,37 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+#define MAX 8
+
+int N, M;
+bool visited[MAX] = { 0 };
+vector<int> answer;
+
+void solve(int depth, int start);
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N >> M;
+	solve(0, -1);
+
+	return 0;
+}
+
+void solve(int depth, int start) {
+	if (depth == M) {
+		for (int i = 0; i < M; i++) {
+			cout << answer[i] << " ";
+		}
+		cout << '\n';
+		return;
+	}
+	else {
+		for (int i = start + 1; i < N; i++) {
+			answer.push_back(i + 1);
+			solve(depth + 1, i);
+			answer.pop_back();
+		}
+	}
+}
