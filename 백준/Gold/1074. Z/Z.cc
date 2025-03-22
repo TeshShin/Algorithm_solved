ad65b73cbf13cf93d4fe5ffae9ccd7429f762e79
Tesh <54969187+TeshShin@users.noreply.github.com>
2025-03-22 11:29:59 +0900

    [Gold V] Title: Z, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1074. Z/Z.cc
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int N;
+
+int Z(int row, int col, int size) {
+	if (size == 1) return 0;
+	int piece = (size * size) / 4;
+	if (row < size / 2) {
+		if (col < size / 2) {
+			return Z(row, col, size / 2);
+		}
+		else {
+			return Z(row, col - size / 2, size / 2) + piece;
+		}
+	}
+	else {
+		if (col < size / 2) {
+			return Z(row - size / 2, col, size / 2) + piece * 2;
+		}
+		else {
+			return Z(row - size / 2, col - size / 2, size / 2) + piece * 3;
+		}
+	}
+}
+
+int main() {
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
+	int row, col;
+	cin >> N >> row >> col;
+	int size = 1;
+	for (int i = 0; i < N; i++) {
+		size *= 2;
+	}
+	cout << Z(row, col, size);
+}
