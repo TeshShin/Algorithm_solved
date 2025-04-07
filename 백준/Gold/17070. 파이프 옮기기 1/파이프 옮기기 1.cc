046f0eda5796c130d81275df3e45f3032aa28945
Tesh <54969187+TeshShin@users.noreply.github.com>
2025-04-07 14:07:27 +0900

    [Gold V] Title: 파이프 옮기기 1, Time: 236 ms, Memory: 39160 KB -BaekjoonHub

---
+++ 백준/Gold/17070. 파이프 옮기기 1/파이프 옮기기 1.cc
@@ -0,0 +1,86 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<utility>
+using namespace std;
+
+int N;
+int home[17][17]; // 0번째 포함 안함
+int dx[3][2] = { {0, 1}, {1, 0}, {1, 1} };
+
+int bfs()
+{
+	int arrives = 0;
+	queue<pair<int,int>> q;
+	queue<int> saveMove;
+	q.push(make_pair(1, 2));
+	saveMove.push(0);
+	while (!q.empty())
+	{
+		pair<int, int> curr = q.front();
+		int currMove = saveMove.front();
+		q.pop();
+		saveMove.pop();
+		// 목적지 도달 시
+		if (curr.first == N && curr.second == N)
+		{
+			arrives++;
+			continue;
+		}
+		// 파이프 움직임
+		for (int i = 0; i < 3; i++)
+		{
+			switch (i)
+			{
+			case 0:
+				if (currMove == 2 || (curr.second + 1 > N) || home[curr.first][curr.second + 1])
+					break;
+				else
+				{
+					q.push(make_pair(curr.first, curr.second + 1));
+					saveMove.push(0);
+				}
+				break;
+			case 1:
+				if ((curr.first + 1 > N || curr.second + 1 > N) 
+					|| home[curr.first + 1][curr.second + 1] 
+					|| home[curr.first][curr.second + 1] 
+					|| home[curr.first + 1][curr.second])
+					break;
+				else
+				{
+					q.push(make_pair(curr.first + 1, curr.second + 1));
+					saveMove.push(1);
+				}
+				break;
+			case 2:
+				if ((currMove == 0|| curr.first + 1 > N) || home[curr.first + 1][curr.second])
+					break;
+				else
+				{
+					q.push(make_pair(curr.first + 1, curr.second));
+					saveMove.push(2);
+				}
+				break;
+			default:
+				break;
+			}
+		}
+	}
+	return arrives;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N;
+	for (int i = 1; i <= N; i++)
+		for (int j = 1; j <= N; j++)
+			cin >> home[i][j];
+
+	cout << bfs();
+
+
+	return 0;
+}
