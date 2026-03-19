2687850babaa3937c0ef7e2da4c353b4c9484f27
Tesh <54969187+TeshShin@users.noreply.github.com>
2026-03-19 16:01:33 +0900

    [Gold IV] Title: 수 묶기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Gold/1744. 수 묶기/수 묶기.cc
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+int N;
+
+bool compare(int i, int j)
+{
+	return i > j;
+}
+
+bool PlusIsBetter(int i, int j)
+{
+	return (i + j) > (i * j);
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	cin >> N;
+	vector<int> positive;
+	vector<int> negative;
+	for (int i = 0; i < N; i++)
+	{
+		int input;
+		cin >> input;
+		if (input > 0)
+			positive.push_back(input);
+		else
+			negative.push_back(input);
+	}
+	sort(positive.begin(), positive.end(), compare);
+	sort(negative.begin(), negative.end());
+	int answer = 0;
+	for (int i = 0; i < positive.size(); i++)
+	{
+		if ((i + 1) >= positive.size() || PlusIsBetter(positive[i], positive[i + 1]))
+		{
+			answer += positive[i];
+		}
+		else
+		{
+			answer += positive[i] * positive[++i];
+		}
+	}
+	for (int i = 0; i < negative.size(); i++)
+	{
+		if ((i + 1) >= negative.size() || PlusIsBetter(negative[i], negative[i + 1]))
+		{
+			answer += negative[i];
+		}
+		else
+		{
+			answer += negative[i] * negative[++i];
+		}
+	}
+
+	cout << answer;
+	return 0;
+}
