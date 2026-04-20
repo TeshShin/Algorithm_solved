b648e78eaba99245cb826ab69dbcf03a55e853d0
Tesh <54969187+TeshShin@users.noreply.github.com>
2026-04-20 17:02:25 +0900

    [level 2] Title: 전화번호 목록, Time: 82.16 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
+++ 프로그래머스/2/42577. 전화번호 목록/전화번호 목록.cpp
@@ -1,24 +1,32 @@
 #include <string>
 #include <vector>
-#include <unordered_set>
+#include <algorithm>
 
 using namespace std;
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
-
-    unordered_set<string> Hash;
-    for(const string Numbers : phone_book)
-        Hash.insert(Numbers);
-
-    for(const string Numbers : phone_book) {
-        string PhoneNumber = "";
-        for(const char Number : Numbers) {
-            PhoneNumber += Number;
-            if(Hash.find(PhoneNumber) != Hash.end() && PhoneNumber != Numbers)
-                answer = false;
+    sort(phone_book.begin(), phone_book.end());
+    for(size_t i = 0; i < phone_book.size() - 1; i++)
+    {
+        string current = phone_book[i];
+        int phoneLen = current.length();
+        string other = phone_book[i + 1];
+        int otherLen = other.length();
+        if(phoneLen >= otherLen) continue;
+        bool canFront = true;
+        for(int k = 0; k < phoneLen; k++)
+        {
+            if(current[k] != other[k])
+            {
+                canFront = false;
+                break;
+            }
+        }
+        if(canFront)
+        {
+            return false;
         }
     }
-
     return answer;
 }
