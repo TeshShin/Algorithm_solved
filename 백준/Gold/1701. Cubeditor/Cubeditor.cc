380ae88e0f8f77938ab52cc217af9270495b7ea9
Tesh <54969187+TeshShin@users.noreply.github.com>
2026-04-22 18:30:07 +0900

    [Gold II] Title: Cubeditor, Time: 76 ms, Memory: 2168 KB -BaekjoonHub

---
+++ 백준/Gold/1701. Cubeditor/Cubeditor.cc
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<vector>
+#include<queue>
+#include<algorithm>
+using namespace std;
+
+vector<int> GetPi(const string& s)
+{
+	int Length = static_cast<int>(s.size());
+	vector<int> Pi(Length, 0);
+	int j = 0;
+	for (int i = 1; i < Length; i++)
+	{
+		while (j > 0 && s[i] != s[j])
+		{
+			j = Pi[j - 1];
+		}
+		if (s[i] == s[j])
+		{
+			j++;
+			Pi[i] = j;
+		}
+	}
+	return Pi;
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	string Input;
+	cin >> Input;
+	int Length = static_cast<int>(Input.size());
+	vector<int> Pi(Length, 0);
+	int Answer = 0;
+	for (int i = 0; i < Length; i++)
+	{
+		string Sub = Input.substr(i);
+		const vector<int>& Pi = GetPi(Sub);
+		for (int Value : Pi)
+		{
+			Answer = max(Value, Answer);
+		}
+	}
+	cout << Answer;
+	return 0;
+}
