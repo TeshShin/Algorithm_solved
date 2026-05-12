9d2490eadeaa295b918101df5bc5560c6d68e632
Tesh <54969187+TeshShin@users.noreply.github.com>
2026-05-12 23:17:04 +0900

    [level 3] Title: 표현 가능한 이진트리, Time: 3.22 ms, Memory: 5.66 MB -BaekjoonHub

--- 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.cpp
+++ 프로그래머스/3/150367. 표현 가능한 이진트리/표현 가능한 이진트리.cpp
@@ -4,6 +4,7 @@
 
 using namespace std;
 using ll = long long;
+// 수를 이진 형태로 만들기
 string MakeBinary(ll num)
 {
     string binary = "";
@@ -12,9 +13,13 @@ string MakeBinary(ll num)
         binary.push_back((num % 2) + '0');
         num /= 2;
     }
+    // push_back으로 앞 자리부터 채워지므로 뒤집어야 함
     reverse(binary.begin(), binary.end());
     return binary;
 }
+// 루트가 0 이면 자식들도 0이어야 한다.
+// 서브트리도 그래야하고, 만약 루트가 0인데 자식에 1이 있다면
+// 해당 수는 이진 트리로 표현할 수 없다.
 bool Check(const string& binary, ll left, ll right)
 {
     if (left >= right)
@@ -36,6 +41,7 @@ bool Check(const string& binary, ll left, ll right)
     return Check(binary, left, mid - 1) && 
         Check(binary, mid + 1, right);
 }
+
 vector<int> solution(vector<long long> numbers) {
     vector<int> answer;
     for(ll num : numbers)
@@ -44,9 +50,11 @@ vector<int> solution(vector<long long> numbers) {
         int targetLength = 1;
         while(targetLength < binary.length())
         {
+            // 이진 트리의 개수인 1, 3, 7, ... 의 꼴이 되어야 한다.
             targetLength = targetLength * 2 + 1;
         }
         ll frontZeroLength = targetLength - binary.length();
+        // 앞의 더미 채우기
         binary = string(frontZeroLength, '0') + binary;
         answer.push_back(Check(binary, 0, targetLength - 1));
     }
