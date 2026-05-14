75238e222fa35c023b48eec5aabbc57c854840c7
Tesh <54969187+TeshShin@users.noreply.github.com>
2026-05-14 22:52:54 +0900

    [level 2] Title: 디펜스 게임, Time: 17.28 ms, Memory: 38.8 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cpp
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.cpp
@@ -1,27 +1,9 @@
 #include <string>
 #include <vector>
-#include<queue>
+
 using namespace std;
 
 int solution(int n, int k, vector<int> enemy) {
     int answer = 0;
-    priority_queue<int> pq;
-    for(int i = 0; i < enemy.size(); i++)
-    {
-        n -= enemy[i];
-        pq.push(enemy[i]);
-        answer++;
-        if(n < 0)
-        {
-            if(k == 0)
-            {
-                return i;
-            }
-            
-            n += pq.top();
-            pq.pop();
-            k--;
-        }
-    }
     return answer;
 }
