e90890c3787c0b2986f671d4ca65fffbe19981a4
Tesh <54969187+TeshShin@users.noreply.github.com>
2026-05-15 21:09:28 +0900

    [level 4] Title: 1,2,3 떨어트리기, Time: 0.10 ms, Memory: 3.94 MB -BaekjoonHub

--- 프로그래머스/4/150364. 1，2，3 떨어트리기/1，2，3 떨어트리기.cpp
+++ 프로그래머스/4/150364. 1，2，3 떨어트리기/1，2，3 떨어트리기.cpp
@@ -4,103 +4,114 @@
 
 using namespace std;
 
-bool Check(const vector<int>& target, const vector<int>& leafCount)
+int Drop(const vector<vector<int>>& children, vector<int>& childrenIndex)
 {
-    for(int i = 1; i <= target.size(); i++)
+    int node = 1;
+
+    while (!children[node].empty())
     {
-        if(leafCount[i] <= target[i - 1] && target[i - 1] <= leafCount[i] * 3)
-        {
-            continue;
-        }
-        else
+        int index = childrenIndex[node];
+        childrenIndex[node] = (index + 1) % children[node].size();
+        node = children[node][index];
+    }
+
+    return node;
+}
+
+bool CanMakeTarget(const vector<int>& target, const vector<int>& leafCount)
+{
+    int nodeCount = target.size();
+
+    for (int node = 1; node <= nodeCount; node++)
+    {
+        int count = leafCount[node];
+        int value = target[node - 1];
+
+        if (value < count || value > count * 3)
         {
             return false;
         }
     }
+
     return true;
 }
 
+vector<int> BuildAnswer(
+    const vector<int>& target,
+    const vector<int>& leafCount,
+    const vector<int>& arriveNodes)
+{
+    int nodeCount = target.size();
 
-vector<int> solution(vector<vector<int>> edges, vector<int> target) {
-    vector<int> answer;
-    // 노드는 1부터니까
-    vector<vector<int>> children(target.size() + 1);
-    vector<int> childrenIndex(target.size() + 1, 0);
-    vector<int> leafCount(target.size() + 1, 0);
-    vector<int> arriveIdx;
-    for(auto& edge : edges)
+    vector<vector<int>> nodeValues(nodeCount + 1);
+    vector<int> pushIndex(nodeCount + 1, 0);
+    vector<int> result;
+
+    for (int node = 1; node <= nodeCount; node++)
+    {
+        int count = leafCount[node];
+        int value = target[node - 1];
+
+        nodeValues[node] = vector<int>(count, 1);
+
+        int left = value - count;
+
+        for (int index = count - 1; index >= 0 && left > 0; index--)
+        {
+            int add = min(2, left);
+            nodeValues[node][index] += add;
+            left -= add;
+        }
+    }
+
+    for (int node : arriveNodes)
+    {
+        int index = pushIndex[node]++;
+        result.push_back(nodeValues[node][index]);
+    }
+
+    return result;
+}
+
+vector<int> solution(vector<vector<int>> edges, vector<int> target)
+{
+    int nodeCount = target.size();
+
+    vector<vector<int>> children(nodeCount + 1);
+    vector<int> childrenIndex(nodeCount + 1, 0);
+    vector<int> leafCount(nodeCount + 1, 0);
+    vector<int> arriveNodes;
+
+    for (const vector<int>& edge : edges)
     {
         int parent = edge[0];
         int child = edge[1];
+
         children[parent].push_back(child);
     }
-    for(int i = 1; i < target.size() + 1; i++)
+
+    for (int node = 1; node <= nodeCount; node++)
     {
-        sort(children[i].begin(), children[i].end());
+        sort(children[node].begin(), children[node].end());
     }
-    while(true)
+
+    while (true)
     {
-        int Idx = 1;
-        while(children[Idx].size() > 0)
+        int leaf = Drop(children, childrenIndex);
+
+        leafCount[leaf]++;
+        arriveNodes.push_back(leaf);
+
+        if (leafCount[leaf] > target[leaf - 1])
         {
-            // 자식으로 내려가기
-            int childIdx = childrenIndex[Idx];
-            // 내려가기 전 현재 노드의 간선 바꾸기
-            childrenIndex[Idx] = (childIdx + 1) % children[Idx].size();
-            Idx = children[Idx][childIdx];
-        }
-        // 현재 Idx 는 리프임
-        leafCount[Idx]++;
-        arriveIdx.push_back(Idx);
-        if(leafCount[Idx] > target[Idx - 1])
-        {
-            // 초과해서 ㅈㅈ
             return {-1};
         }
-        else if(leafCount[Idx] <= target[Idx - 1] && target[Idx - 1] <= leafCount[Idx] * 3)
+
+        if (CanMakeTarget(target, leafCount))
         {
-            if(Check(target, leafCount))
-            {
-                // 이땐 걍 되는 거임.
-                break;
-            }
+            break;
         }
     }
-    vector<vector<int>> nodePush(target.size() + 1);
-    for(int i = 1; i <= target.size(); i++)
-    {
-        int value = target[i - 1];
-        int visitedCount = leafCount[i];
-        for(int j = 1; j <= visitedCount; j++)
-        {
-            nodePush[i].push_back(1);
-        }
-        int left = value - visitedCount;
-        
-        int curr = 0;
-        while(left > 0 && curr < visitedCount)
-        {
-            if(left >= 2)
-            {
-                nodePush[i][curr] = 3;
-                left -= 2;
-            }
-            else if(left == 1)
-            {
-                nodePush[i][curr] = 2;
-                left -= 1;
-            }
-            curr++;
-        }
-        reverse(nodePush[i].begin(), nodePush[i].end());
-    }
-    vector<int> pushIdx(target.size() + 1, 0);
-    for(int i = 0; i < arriveIdx.size(); i++)
-    {
-        int currentNode = arriveIdx[i];
-        int Idx = pushIdx[currentNode];
-        answer.push_back(nodePush[currentNode][Idx]);
-        pushIdx[currentNode]++;
-    }
-    return answer;
+
+    return BuildAnswer(target, leafCount, arriveNodes);
 }
