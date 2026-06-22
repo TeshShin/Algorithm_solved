84cc14bfdb97c5098d46d19ac9e54c03cf78313e
Tesh <54969187+TeshShin@users.noreply.github.com>
2026-06-22 21:18:30 +0900

    [level 3] Title: 숫자 타자 대회, Time: 134.63 ms, Memory: 4.63 MB -BaekjoonHub

--- 프로그래머스/3/136797. 숫자 타자 대회/숫자 타자 대회.cpp
+++ 프로그래머스/3/136797. 숫자 타자 대회/숫자 타자 대회.cpp
@@ -1,83 +1,121 @@
 #include <string>
 #include <vector>
+#include <algorithm>
+#include <utility>
 
 using namespace std;
+
 using vvi = vector<vector<int>>;
-using vvvi = vector<vvi>;
-constexpr int INF = 700'001;
-int calcCost(int from, int to, const vector<pair<int, int>>& pos)
+
+constexpr int INF = 1'000'000'000;
+
+int CalcCost(int from, int to, const vector<pair<int, int>>& positions)
 {
-    if(from == to)
+    if (from == to)
     {
         return 1;
     }
-    else
-    {
-        int diffCol = abs(pos[from].second - pos[to].second);
-        int diffRow = abs(pos[from].first - pos[to].first);
-        int result = min(diffCol, diffRow) * 3 
-            + (max(diffCol, diffRow) - min(diffCol, diffRow)) * 2;
-        return result;
-    }
+
+    int rowDiff = abs(positions[from].first - positions[to].first);
+    int colDiff = abs(positions[from].second - positions[to].second);
+
+    int diagonal = min(rowDiff, colDiff);
+    int straight = max(rowDiff, colDiff) - diagonal;
+
+    return diagonal * 3 + straight * 2;
 }
 
-int solution(string numbers) {
+vector<pair<int, int>> MakePositions()
+{
+    vector<pair<int, int>> positions(10);
+
+    for (int number = 1; number <= 9; ++number)
+    {
+        positions[number] = {(number - 1) / 3, (number - 1) % 3};
+    }
+
+    positions[0] = {3, 1};
+
+    return positions;
+}
+
+vvi MakeCostTable(const vector<pair<int, int>>& positions)
+{
+    vvi cost(10, vector<int>(10, 0));
+
+    for (int from = 0; from <= 9; ++from)
+    {
+        for (int to = 0; to <= 9; ++to)
+        {
+            cost[from][to] = CalcCost(from, to, positions);
+        }
+    }
+
+    return cost;
+}
+
+int solution(string numbers)
+{
     int answer = INF;
-    vector<pair<int, int>> pos(10);
-    for(int i = 0; i < 10; i++)
+
+    vector<pair<int, int>> positions = MakePositions();
+    vvi cost = MakeCostTable(positions);
+
+    vvi currentDp(10, vector<int>(10, INF));
+    vvi nextDp(10, vector<int>(10, INF));
+
+    currentDp[4][6] = 0;
+
+    for (char digit : numbers)
     {
-        if(i == 0)
+        int number = digit - '0';
+
+        for (int left = 0; left <= 9; ++left)
         {
-            pos[i] = {3, 1};
+            fill(nextDp[left].begin(), nextDp[left].end(), INF);
         }
-        else
+
+        for (int left = 0; left <= 9; ++left)
         {
-            pos[i] = {(i - 1) / 3, (i - 1) % 3};
-        }
-    }
-    vvi cost(10, vector<int>(10));
-    for(int i = 0; i < 10; i++)
-    {
-        for(int j = i; j < 10; j++)
-        {
-            cost[i][j] = calcCost(i, j, pos);
-            cost[j][i] = cost[i][j];
-        }
-    }
-    
-    int n = numbers.length();
-    vvvi dp (n + 1, vector<vector<int>> (10, vector<int> (10, INF)));
-    dp[0][4][6] = 0;
-    for(int i = 0; i < n; i++)
-    {
-        int num = numbers[i] - '0';
-        // 해당 인덱스를 왼손, 오른손 각각으로
-        for(int l = 0; l <= 9; l++)
-        {
-            for(int r = 0; r <= 9; r++)
+            for (int right = 0; right <= 9; ++right)
             {
-                if(dp[i][l][r] == INF) continue;
-                if(num == l || num == r) // l과 r이 같아지는 상황을 제외
+                if (currentDp[left][right] == INF)
                 {
-                    dp[i + 1][l][r] = min(dp[i + 1][l][r], dp[i][l][r] + 1);
+                    continue;
+                }
+
+                if (number == left || number == right)
+                {
+                    nextDp[left][right] = min(
+                        nextDp[left][right],
+                        currentDp[left][right] + 1
+                    );
                 }
                 else
                 {
-                    dp[i + 1][num][r] = min(dp[i + 1][num][r], dp[i][l][r] + cost[l][num]);
-                    dp[i + 1][l][num] = min(dp[i + 1][l][num], dp[i][l][r] + cost[r][num]);
+                    nextDp[number][right] = min(
+                        nextDp[number][right],
+                        currentDp[left][right] + cost[left][number]
+                    );
+
+                    nextDp[left][number] = min(
+                        nextDp[left][number],
+                        currentDp[left][right] + cost[right][number]
+                    );
                 }
             }
         }
+
+        currentDp = nextDp;
     }
-    for(int l = 0; l <= 9; l++)
+
+    for (int left = 0; left <= 9; ++left)
     {
-        for(int r = 0; r <= 9; r++)
+        for (int right = 0; right <= 9; ++right)
         {
-            if(dp[n][l][r] < answer)
-            {
-                answer = dp[n][l][r];
-            }
+            answer = min(answer, currentDp[left][right]);
         }
     }
+
     return answer;
 }
