991931c233d0a2cd423cdc01d2288ceaddff911c
Tesh <54969187+TeshShin@users.noreply.github.com>
2026-06-30 21:31:39 +0900

    [level 3] Title: 자물쇠와 열쇠, Time: 15.26 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.cpp
+++ 프로그래머스/3/60059. 자물쇠와 열쇠/자물쇠와 열쇠.cpp
@@ -2,7 +2,7 @@
 #include <vector>
 
 using namespace std;
-
+// key 회전
 vector<vector<int>> Rotate(const vector<vector<int>>& key, int M)
 {
     vector<vector<int>> rotated(M, vector<int>(M, 0));
@@ -17,6 +17,8 @@ vector<vector<int>> Rotate(const vector<vector<int>>& key, int M)
 
     return rotated;
 }
+
+// 자물쇠와 열쇠가 맞는지
 bool IsUnlocked(const vector<vector<int>>& board, int lockOffset, int N)
 {
     for (int row = 0; row < N; ++row)
@@ -37,12 +39,13 @@ bool solution(vector<vector<int>> key, vector<vector<int>> lock)
 {
     int M = key.size();
     int N = lock.size();
-
+    // 초기 key의 끝이 lock의 시작
     int lockOffset = M - 1;
     int boardSize = N + lockOffset * 2;
 
     vector<vector<int>> board(boardSize, vector<int>(boardSize, 0));
-
+    
+    // 보드의 중앙인 lock 채우기
     for (int row = 0; row < N; ++row)
     {
         for (int col = 0; col < N; ++col)
@@ -51,12 +54,13 @@ bool solution(vector<vector<int>> key, vector<vector<int>> lock)
         }
     }
 
-    for (int rot = 0; rot < 4; ++rot)
+    for (int rot = 0; rot < 4; ++rot) // 회전 4번
     {
         for (int startRow = 0; startRow <= boardSize - M; ++startRow)
         {
             for (int startCol = 0; startCol <= boardSize - M; ++startCol)
             {
+                // 보드에 키 값 더하기
                 for (int row = 0; row < M; ++row)
                 {
                     for (int col = 0; col < M; ++col)
@@ -64,12 +68,13 @@ bool solution(vector<vector<int>> key, vector<vector<int>> lock)
                         board[startRow + row][startCol + col] += key[row][col];
                     }
                 }
-
+                // lock 부분만 체크하면 됨. lock이 모두 1 이면 가능
+                // 0이거나 2가 있으면 불가
                 if (IsUnlocked(board, lockOffset, N))
                 {
                     return true;
                 }
-
+                // 더했던 key 되돌리기
                 for (int row = 0; row < M; ++row)
                 {
                     for (int col = 0; col < M; ++col)
@@ -79,7 +84,7 @@ bool solution(vector<vector<int>> key, vector<vector<int>> lock)
                 }
             }
         }
-
+        // 회전
         key = Rotate(key, M);
     }
 
